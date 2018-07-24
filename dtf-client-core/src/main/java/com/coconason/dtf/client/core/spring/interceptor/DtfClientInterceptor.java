64b54a1565b1ee56d104518e060b7715907e7fca
wgy8283335 <wgy8283335@126.com>
2018-07-24 09:34:50 +0800

    initialization

---
+++ dtf-client-core/src/main/java/com/coconason/dtf/client/core/spring/interceptor/DtfClientInterceptor.java
@@ -0,0 +1,15 @@
+package com.coconason.dtf.client.core.spring.interceptor;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author: Jason
+ * @date: 2018/7/24-9:07
+ */
+@Component
+public class DtfClientInterceptor {
+
+    public Object around(ProceedingJoinPoint point) throws Throwable{
+        return null;
+    }
+}
