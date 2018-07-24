d4c61bf90e17567bb015a0f4d21c6c38b410899c
wgy8283335 <wgy8283335@126.com>
2018-07-24 09:35:09 +0800

    initialization

--- dtf-client-core/src/main/java/com/coconason/dtf/client/core/annotation/DtfTransaction.java
+++ dtf-client-core/src/main/java/com/coconason/dtf/client/core/annotation/DtfTransaction.java
@@ -1,9 +1,6 @@
 package com.coconason.dtf.client.core.annotation;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
  * @Author Jason
@@ -11,7 +8,7 @@ import java.lang.annotation.Target;
  */
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-
+@Inherited
 public @interface DtfTransaction {
 
 }
