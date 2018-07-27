8e7bff058263183c9086bec9139fcf218ea37212
wgy8283335 <wgy8283335@126.com>
2018-07-27 16:41:03 +0800

    initialization

---
+++ dtf-common/src/main/java/com/coconason/dtf/common/codec/MarshallingCodecFactory.java
@@ -0,0 +1,32 @@
+package com.coconason.dtf.common.codec;
+
+import java.io.IOException;
+
+import org.jboss.marshalling.Marshaller;
+import org.jboss.marshalling.MarshallerFactory;
+import org.jboss.marshalling.Marshalling;
+import org.jboss.marshalling.MarshallingConfiguration;
+import org.jboss.marshalling.Unmarshaller;
+
+public final class MarshallingCodecFactory {
+
+    protected static Marshaller buildMarshalling() throws IOException {
+	final MarshallerFactory marshallerFactory = Marshalling
+		.getProvidedMarshallerFactory("serial");
+	final MarshallingConfiguration configuration = new MarshallingConfiguration();
+	configuration.setVersion(5);
+	Marshaller marshaller = marshallerFactory
+		.createMarshaller(configuration);
+	return marshaller;
+    }
+
+    protected static Unmarshaller buildUnMarshalling() throws IOException {
+	final MarshallerFactory marshallerFactory = Marshalling
+		.getProvidedMarshallerFactory("serial");
+	final MarshallingConfiguration configuration = new MarshallingConfiguration();
+	configuration.setVersion(5);
+	final Unmarshaller unmarshaller = marshallerFactory
+		.createUnmarshaller(configuration);
+	return unmarshaller;
+    }
+}
