58bb36731ce03278ec2fc7e1b9c0a1003ed2bc36
wgy8283335 <wgy8283335@126.com>
2018-07-27 17:01:27 +0800

    initialization

--- dtf-common/src/main/java/com/coconason/dtf/common/codec/MarshallingEncoder.java
+++ dtf-common/src/main/java/com/coconason/dtf/common/codec/MarshallingEncoder.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2013-2018 Lilinfeng.
- *  
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *  
- *      http://www.apache.org/licenses/LICENSE-2.0
- *  
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.coconason.dtf.common.codec;
 
 import io.netty.buffer.ByteBuf;
@@ -29,20 +14,20 @@ public class MarshallingEncoder {
     Marshaller marshaller;
 
     public MarshallingEncoder() throws IOException {
-	marshaller = MarshallingCodecFactory.buildMarshalling();
+		marshaller = MarshallingCodecFactory.buildMarshalling();
     }
 
     protected void encode(Object msg, ByteBuf out) throws Exception {
-	try {
-	    int lengthPos = out.writerIndex();
-	    out.writeBytes(LENGTH_PLACEHOLDER);
-	    ChannelBufferByteOutput output = new ChannelBufferByteOutput(out);
-	    marshaller.start(output);
-	    marshaller.writeObject(msg);
-	    marshaller.finish();
-	    out.setInt(lengthPos, out.writerIndex() - lengthPos - 4);
-	} finally {
-	    marshaller.close();
-	}
+		try {
+			int lengthPos = out.writerIndex();
+			out.writeBytes(LENGTH_PLACEHOLDER);
+			ChannelBufferByteOutput output = new ChannelBufferByteOutput(out);
+			marshaller.start(output);
+			marshaller.writeObject(msg);
+			marshaller.finish();
+			out.setInt(lengthPos, out.writerIndex() - lengthPos - 4);
+		} finally {
+			marshaller.close();
+		}
     }
 }
--- dtf-common/src/main/java/com/coconason/dtf/common/codec/NettyMessageEncoder.java
+++ dtf-common/src/main/java/com/coconason/dtf/common/codec/NettyMessageEncoder.java
@@ -7,11 +7,9 @@ import io.netty.handler.codec.MessageToByteEncoder;
 
 import java.io.IOException;
 import java.util.Map;
-import io.netty.handler.codec.marshalling.MarshallingEncoder;
 
 
-public final class NettyMessageEncoder extends
-	MessageToByteEncoder<NettyMessage> {
+public final class NettyMessageEncoder extends MessageToByteEncoder<NettyMessage> {
 
     MarshallingEncoder marshallingEncoder;
 
@@ -20,35 +18,35 @@ public final class NettyMessageEncoder extends
     }
 
     @Override
-    protected void encode(ChannelHandlerContext ctx, NettyMessage msg,
-	    ByteBuf sendBuf) throws Exception {
-	if (msg == null || msg.getHeader() == null)
-	    throw new Exception("The encode message is null");
-	sendBuf.writeInt((msg.getHeader().getCrcCode()));
-	sendBuf.writeInt((msg.getHeader().getLength()));
-	sendBuf.writeLong((msg.getHeader().getSessionID()));
-	sendBuf.writeByte((msg.getHeader().getType()));
-	sendBuf.writeByte((msg.getHeader().getPriority()));
-	sendBuf.writeInt((msg.getHeader().getAttachment().size()));
-	String key = null;
-	byte[] keyArray = null;
-	Object value = null;
-	for (Map.Entry<String, Object> param : msg.getHeader().getAttachment()
-		.entrySet()) {
-	    key = param.getKey();
-	    keyArray = key.getBytes("UTF-8");
-	    sendBuf.writeInt(keyArray.length);
-	    sendBuf.writeBytes(keyArray);
-	    value = param.getValue();
-	    marshallingEncoder.encode(value, sendBuf);
-	}
-	key = null;
-	keyArray = null;
-	value = null;
-	if (msg.getBody() != null) {
-	    marshallingEncoder.encode(msg.getBody(), sendBuf);
-	}else
-	    sendBuf.writeInt(0);
-	sendBuf.setInt(4, sendBuf.readableBytes() - 8);
+    protected void encode(ChannelHandlerContext ctx, NettyMessage msg,ByteBuf sendBuf) throws Exception {
+		if (msg == null || msg.getHeader() == null) {
+			throw new Exception("The encode message is null");
+		}
+		sendBuf.writeInt((msg.getHeader().getCrcCode()));
+		sendBuf.writeInt((msg.getHeader().getLength()));
+		sendBuf.writeLong((msg.getHeader().getSessionID()));
+		sendBuf.writeByte((msg.getHeader().getType()));
+		sendBuf.writeByte((msg.getHeader().getPriority()));
+		sendBuf.writeInt((msg.getHeader().getAttachment().size()));
+		String key = null;
+		byte[] keyArray = null;
+		Object value = null;
+		for (Map.Entry<String, Object> param : msg.getHeader().getAttachment().entrySet()) {
+			key = param.getKey();
+			keyArray = key.getBytes("UTF-8");
+			sendBuf.writeInt(keyArray.length);
+			sendBuf.writeBytes(keyArray);
+			value = param.getValue();
+			marshallingEncoder.encode(value, sendBuf);
+		}
+		key = null;
+		keyArray = null;
+		value = null;
+		if (msg.getBody() != null) {
+			marshallingEncoder.encode(msg.getBody(), sendBuf);
+		}else{
+			sendBuf.writeInt(0);
+		}
+		sendBuf.setInt(4, sendBuf.readableBytes() - 8);
     }
 }
