1e472e56c669a7d546c2cc6555a12361ebb7b5e9
wgy8283335 <wgy8283335@126.com>
2018-08-07 11:09:29 +0800

    initialization

--- spring-demo/src/main/java/com/coconason/dtf/demo/service/impl/DemoServiceImpl.java
+++ spring-demo/src/main/java/com/coconason/dtf/demo/service/impl/DemoServiceImpl.java
@@ -4,6 +4,7 @@ package com.coconason.dtf.demo.service.impl;
 import com.coconason.dtf.client.core.annotation.DtfTransaction;
 import com.coconason.dtf.demo.service.IDemoService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @Author: Jason
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class DemoServiceImpl implements IDemoService{
     @DtfTransaction
+    @Transactional
     @Override
     public String getDemoById(String id){
         System.out.println(id+" in Service");
