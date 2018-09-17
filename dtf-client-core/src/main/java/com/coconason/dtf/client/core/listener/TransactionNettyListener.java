adea497dbf005a6c61a484a5e369bd7393f7f7a0
wgy8283335 <wgy8283335@126.com>
2018-09-17 10:52:42 +0800

    initialization

--- dtf-client-core/src/main/java/com/coconason/dtf/client/core/listener/TransactionNettyListener.java
+++ dtf-client-core/src/main/java/com/coconason/dtf/client/core/listener/TransactionNettyListener.java
@@ -2,30 +2,28 @@ package com.coconason.dtf.client.core.listener;
 
 import com.coconason.dtf.client.core.nettyclient.messagequeue.TransactionMessageSender;
 import com.coconason.dtf.client.core.nettyclient.protobufclient.NettyService;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-import org.springframework.stereotype.Component;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Service;
 
 /**
  * @Author: Jason
  * @date: 2018/8/22-16:46
  */
-@Component
-public class TransactionNettyListener implements ApplicationContextAware{
+
+@Service
+public class TransactionNettyListener implements ApplicationListener<ContextRefreshedEvent>{
 
     @Autowired
     NettyService nettyService;
 
     @Autowired
     TransactionMessageSender sender;
-
     @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException{
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
         nettyService.start();
         try{
-            Thread.sleep(5000);
             sender.startSendMessage();
         }catch (Exception e){
             e.printStackTrace();
