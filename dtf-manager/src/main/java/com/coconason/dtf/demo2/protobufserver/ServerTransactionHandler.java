fd1817b95be5c2eaef1a34f8eb94eaf10d6e945b
wgy8283335 <wgy8283335@126.com>
2018-09-19 10:37:01 +0800

    initialization

--- dtf-manager/src/main/java/com/coconason/dtf/demo2/protobufserver/ServerTransactionHandler.java
+++ dtf-manager/src/main/java/com/coconason/dtf/demo2/protobufserver/ServerTransactionHandler.java
@@ -3,7 +3,6 @@ package com.coconason.dtf.demo2.protobufserver;
 import com.alibaba.fastjson.JSONObject;
 import com.coconason.dtf.common.protobuf.MessageProto;
 import com.coconason.dtf.common.protobuf.MessageProto.Message.ActionType;
-import com.coconason.dtf.common.utils.UuidGenerator;
 import com.coconason.dtf.demo2.cache.MessageAsyncCache;
 import com.coconason.dtf.demo2.cache.MessageAsyncQueue;
 import com.coconason.dtf.demo2.cache.MessageForSubmitSyncCache;
@@ -12,14 +11,14 @@ import com.coconason.dtf.demo2.message.TransactionMessageForAdding;
 import com.coconason.dtf.demo2.message.TransactionMessageForSubmit;
 import com.coconason.dtf.demo2.message.TransactionMessageGroup;
 import com.coconason.dtf.demo2.message.TransactionMessageGroupAsync;
+import com.coconason.dtf.demo2.service.ApplyForRunnable;
+import com.coconason.dtf.demo2.utils.MessageSender;
 import com.coconason.dtf.demo2.service.SendRunnable;
 import com.coconason.dtf.demo2.threadpools.ThreadPoolForServer;
-import com.coconason.dtf.demo2.utils.SetUtil;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
 import java.util.List;
-import java.util.Set;
 
 /**
  * @Author: Jason
@@ -69,24 +68,24 @@ public class ServerTransactionHandler extends ChannelInboundHandlerAdapter{
                 //store the message in the cache.
                 //check whether the group exits in the cache
                 messageSyncCache.putDependsOnCondition(new TransactionMessageGroup(message,ctx));
-                threadPoolForServer.addTask(new ApplyForRunnable(message,ActionType.ADD,ctx));
+                threadPoolForServer.addTask(new ApplyForRunnable(message,ActionType.ADD,ctx, messageForSubmitSyncCache, messageSyncCache));
                 break;
             case APPLYFORSUBMIT:
                 messageForSubmitSyncCache.put( new TransactionMessageForSubmit(message));
-                threadPoolForServer.addTask(new ApplyForRunnable(message,ActionType.APPROVESUBMIT,ctx));
+                threadPoolForServer.addTask(new ApplyForRunnable(message,ActionType.APPROVESUBMIT,ctx, messageForSubmitSyncCache, messageSyncCache));
                 break;
             case ADD_STRONG:
                 //store the message in the cache.
                 //check whether the group exits in the cache
                 messageSyncCache.putDependsOnCondition(new TransactionMessageGroup(message,ctx));
-                threadPoolForServer.addTask(new ApplyForRunnable(message,ActionType.ADD_STRONG,ctx));
+                threadPoolForServer.addTask(new ApplyForRunnable(message,ActionType.ADD_STRONG,ctx, messageForSubmitSyncCache, messageSyncCache));
                 break;
             case APPLYFORSUBMIT_STRONG:
                 messageForSubmitSyncCache.put( new TransactionMessageForSubmit(message));
-                threadPoolForServer.addTask(new ApplyForRunnable(message,ActionType.APPROVESUBMIT_STRONG,ctx));
+                threadPoolForServer.addTask(new ApplyForRunnable(message,ActionType.APPROVESUBMIT_STRONG,ctx, messageForSubmitSyncCache, messageSyncCache));
                 break;
             case CANCEL:
-                threadPoolForServer.addTask(new ApplyForRunnable(message,ActionType.CANCEL,ctx));
+                threadPoolForServer.addTask(new ApplyForRunnable(message,ActionType.CANCEL,ctx, messageForSubmitSyncCache, messageSyncCache));
             case SUB_SUCCESS_STRONG:
                 String memberId = JSONObject.parseObject(message.getInfo()).get("memberId").toString();
                 TransactionMessageGroup groupTemp = messageSyncCache.get(JSONObject.parseObject(message.getInfo()).get("groupId").toString());
@@ -105,13 +104,13 @@ public class ServerTransactionHandler extends ChannelInboundHandlerAdapter{
                     }
                 }
                 if(flag == true){
-                    snedMsg(groupTemp.getGroupId(),ActionType.WHOLE_SUCCESS_STRONG,groupTemp.getCtxForSubmitting());
+                    MessageSender.snedMsg(groupTemp.getGroupId(),ActionType.WHOLE_SUCCESS_STRONG,groupTemp.getCtxForSubmitting());
                     messageSyncCache.clear(groupTemp.getGroupId());
                 }
                 break;
             case SUB_FAIL_STRONG:
                 TransactionMessageGroup groupTemp1 = messageSyncCache.get(JSONObject.parseObject(message.getInfo()).get("groupId").toString());
-                snedMsg(groupTemp1.getGroupId(),ActionType.WHOLE_FAIL_STRONG,groupTemp1.getCtxForSubmitting());
+                MessageSender.snedMsg(groupTemp1.getGroupId(),ActionType.WHOLE_FAIL_STRONG,groupTemp1.getCtxForSubmitting());
                 messageSyncCache.clear(groupTemp1.getGroupId());
                 break;
             case ADD_ASYNC:
@@ -119,9 +118,9 @@ public class ServerTransactionHandler extends ChannelInboundHandlerAdapter{
                 try{
                     transactionMessageGroupAsync = TransactionMessageGroupAsync.parse(message);
                     messageAsyncCache.putDependsOnConditionAsync(transactionMessageGroupAsync);
-                    snedMsg(transactionMessageGroupAsync.getGroupId(),ActionType.ADD_SUCCESS_ASYNC,ctx);
+                    MessageSender.snedMsg(transactionMessageGroupAsync.getGroupId(),ActionType.ADD_SUCCESS_ASYNC,ctx);
                 }catch (Exception e) {
-                    snedMsg(transactionMessageGroupAsync.getGroupId(),ActionType.ADD_FAIL_ASYNC,ctx);
+                    MessageSender.snedMsg(transactionMessageGroupAsync.getGroupId(),ActionType.ADD_FAIL_ASYNC,ctx);
                 }
                 break;
             case ASYNC_COMMIT:
@@ -135,63 +134,63 @@ public class ServerTransactionHandler extends ChannelInboundHandlerAdapter{
         ctx.fireChannelRead(msg);
     }
 
-    private void snedMsg(String groupId,ActionType action,ChannelHandlerContext ctx) throws Exception{
-        MessageProto.Message.Builder builder= MessageProto.Message.newBuilder();
-        JSONObject info = new JSONObject();
-        info.put("groupId",groupId);
-        builder.setInfo(info.toJSONString());
-        builder.setId(UuidGenerator.generateUuid());
-        builder.setAction(action);
-        MessageProto.Message message = builder.build();
-        System.out.println("Send transaction message:\n" + message);
-        ctx.writeAndFlush(message);
-    }
-
-    class ApplyForRunnable implements Runnable{
-
-        private MessageProto.Message message;
-
-        private ActionType actionType;
-
-        private ChannelHandlerContext ctx;
-
-        public ApplyForRunnable(MessageProto.Message message, ActionType actionType, ChannelHandlerContext ctx) {
-            this.message = message;
-            this.actionType = actionType;
-            this.ctx = ctx;
-        }
-
-        @Override
-        public void run() {
-            try{
-                JSONObject info = JSONObject.parseObject(message.getInfo());
-                String groupId = info.get("groupId").toString();
-                Object obj = info.get("groupMemberSet");
-                TransactionMessageForSubmit tmfs = obj == null ? messageForSubmitSyncCache.get(groupId):new TransactionMessageForSubmit(message);
-                if(tmfs == null||tmfs.getMemberSet().isEmpty()||messageSyncCache.get(tmfs.getGroupId())==null){
-                    return;
-                }
-                Set setFromMessage =tmfs.getMemberSet();
-                TransactionMessageGroup elementFromCache = messageSyncCache.get(tmfs.getGroupId());
-                Set setFromCache = elementFromCache.getMemberSet();
-                elementFromCache.setCtxForSubmitting(ctx);
-                messageSyncCache.put(elementFromCache.getGroupId(),elementFromCache);
-                List<TransactionMessageForAdding> memberList = elementFromCache.getMemberList();
-                //check whether the member from message has the same element as the member from cache.
-                if(!setFromMessage.isEmpty()){
-                    if(SetUtil.isSetEqual(setFromMessage,setFromCache)){
-                        for (TransactionMessageForAdding messageForAdding: memberList) {
-                            //success
-                            snedMsg(elementFromCache.getGroupId()+messageForAdding.getGroupMemberId(),ActionType.APPROVESUBMIT_STRONG,messageForAdding.getCtx());
-                        }
-                        if(actionType == ActionType.ADD || actionType == ActionType.APPROVESUBMIT){
-                            messageSyncCache.clear(tmfs.getGroupId());
-                        }
-                    }
-                }
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-        }
-    }
+//    class ApplyForRunnable implements Runnable{
+//
+//        private MessageProto.Message message;
+//
+//        private ActionType actionType;
+//
+//        private ChannelHandlerContext ctx;
+//
+//        public ApplyForRunnable(MessageProto.Message message, ActionType actionType, ChannelHandlerContext ctx) {
+//            this.message = message;
+//            this.actionType = actionType;
+//            this.ctx = ctx;
+//        }
+//
+//        @Override
+//        public void run() {
+//            try{
+//                JSONObject info = JSONObject.parseObject(message.getInfo());
+//                String groupId = info.get("groupId").toString();
+//                Object obj = info.get("groupMemberSet");
+//                TransactionMessageForSubmit tmfs = obj == null ? messageForSubmitSyncCache.get(groupId):new TransactionMessageForSubmit(message);
+//                if(tmfs == null||tmfs.getMemberSet().isEmpty()||messageSyncCache.get(tmfs.getGroupId())==null){
+//                    return;
+//                }
+//                Set setFromMessage =tmfs.getMemberSet();
+//                TransactionMessageGroup elementFromCache = messageSyncCache.get(tmfs.getGroupId());
+//                Set setFromCache = elementFromCache.getMemberSet();
+//                elementFromCache.setCtxForSubmitting(ctx);
+//                messageSyncCache.put(elementFromCache.getGroupId(),elementFromCache);
+//                List<TransactionMessageForAdding> memberList = elementFromCache.getMemberList();
+//                //check whether the member from message has the same element as the member from cache.
+//                if(!setFromMessage.isEmpty()){
+//                    if(SetUtil.isSetEqual(setFromMessage,setFromCache)){
+//                        for (TransactionMessageForAdding messageForAdding: memberList) {
+//                            //success
+//                            snedMsg(elementFromCache.getGroupId()+messageForAdding.getGroupMemberId(),ActionType.APPROVESUBMIT_STRONG,messageForAdding.getCtx());
+//                        }
+//                        if(actionType == ActionType.ADD || actionType == ActionType.APPROVESUBMIT){
+//                            messageSyncCache.clear(tmfs.getGroupId());
+//                        }
+//                    }
+//                }
+//            }catch (Exception e){
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+//
+//    private void snedMsg(String groupId,ActionType action,ChannelHandlerContext ctx) throws Exception{
+//        MessageProto.Message.Builder builder= MessageProto.Message.newBuilder();
+//        JSONObject info = new JSONObject();
+//        info.put("groupId",groupId);
+//        builder.setInfo(info.toJSONString());
+//        builder.setId(UuidGenerator.generateUuid());
+//        builder.setAction(action);
+//        MessageProto.Message message = builder.build();
+//        System.out.println("Send transaction message:\n" + message);
+//        ctx.writeAndFlush(message);
+//    }
 }
---
+++ dtf-manager/src/main/java/com/coconason/dtf/demo2/service/ApplyForRunnable.java
@@ -0,0 +1,74 @@
+package com.coconason.dtf.demo2.service;
+
+import com.alibaba.fastjson.JSONObject;
+import com.coconason.dtf.common.protobuf.MessageProto;
+import com.coconason.dtf.common.protobuf.MessageProto.Message.ActionType;
+import com.coconason.dtf.demo2.cache.MessageForSubmitSyncCache;
+import com.coconason.dtf.demo2.cache.MessageSyncCache;
+import com.coconason.dtf.demo2.message.TransactionMessageForAdding;
+import com.coconason.dtf.demo2.message.TransactionMessageForSubmit;
+import com.coconason.dtf.demo2.message.TransactionMessageGroup;
+import com.coconason.dtf.demo2.utils.MessageSender;
+import com.coconason.dtf.demo2.utils.SetUtil;
+import io.netty.channel.ChannelHandlerContext;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @Author: Jason
+ * @date: 2018/9/19-10:26
+ */
+public class ApplyForRunnable implements Runnable{
+
+    private MessageProto.Message message;
+
+    private ActionType actionType;
+
+    private ChannelHandlerContext ctx;
+
+    private MessageForSubmitSyncCache messageForSubmitSyncCache;
+
+    private MessageSyncCache messageSyncCache;
+
+    public ApplyForRunnable(MessageProto.Message message, ActionType actionType, ChannelHandlerContext ctx,MessageForSubmitSyncCache messageForSubmitSyncCache,MessageSyncCache messageSyncCache) {
+        this.message = message;
+        this.actionType = actionType;
+        this.ctx = ctx;
+        this.messageForSubmitSyncCache = messageForSubmitSyncCache;
+        this.messageSyncCache = messageSyncCache;
+    }
+
+    @Override
+    public void run() {
+        try{
+            JSONObject info = JSONObject.parseObject(message.getInfo());
+            String groupId = info.get("groupId").toString();
+            Object obj = info.get("groupMemberSet");
+            TransactionMessageForSubmit tmfs = obj == null ? messageForSubmitSyncCache.get(groupId):new TransactionMessageForSubmit(message);
+            if(tmfs == null||tmfs.getMemberSet().isEmpty()||messageSyncCache.get(tmfs.getGroupId())==null){
+                return;
+            }
+            Set setFromMessage =tmfs.getMemberSet();
+            TransactionMessageGroup elementFromCache = messageSyncCache.get(tmfs.getGroupId());
+            Set setFromCache = elementFromCache.getMemberSet();
+            elementFromCache.setCtxForSubmitting(ctx);
+            messageSyncCache.put(elementFromCache.getGroupId(),elementFromCache);
+            List<TransactionMessageForAdding> memberList = elementFromCache.getMemberList();
+            //check whether the member from message has the same element as the member from cache.
+            if(!setFromMessage.isEmpty()){
+                if(SetUtil.isSetEqual(setFromMessage,setFromCache)){
+                    for (TransactionMessageForAdding messageForAdding: memberList) {
+                        //success
+                        MessageSender.snedMsg(elementFromCache.getGroupId()+messageForAdding.getGroupMemberId(),ActionType.APPROVESUBMIT_STRONG,messageForAdding.getCtx());
+                    }
+                    if(actionType == ActionType.ADD || actionType == ActionType.APPROVESUBMIT){
+                        messageSyncCache.clear(tmfs.getGroupId());
+                    }
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
