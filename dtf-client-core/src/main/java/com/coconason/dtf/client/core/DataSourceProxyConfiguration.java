a14d4af6d8db032915320fe5b9b2c3275d925749
wgy8283335 <wgy8283335@126.com>
2018-09-21 11:45:11 +0800

    initialization

--- dtf-client-core/src/main/java/com/coconason/dtf/client/core/DataSourceProxyConfiguration.java
+++ dtf-client-core/src/main/java/com/coconason/dtf/client/core/DataSourceProxyConfiguration.java
@@ -21,22 +21,25 @@ public class DataSourceProxyConfiguration{
 
 	@Bean
 	public DataSource dataSource() {
-		DruidDataSource dataSource = new DruidDataSource();
-		dataSource.setUrl(env.getProperty("spring.datasource.url"));
-		dataSource.setUsername(env.getProperty("spring.datasource.username"));//用户名
-		dataSource.setPassword(env.getProperty("spring.datasource.password"));//密码
-		dataSource.setInitialSize(2);
-		dataSource.setMaxActive(20);
-		dataSource.setMinIdle(0);
-		dataSource.setMaxWait(60000);
-		dataSource.setValidationQuery("SELECT 1");
-		dataSource.setTestOnBorrow(false);
-		dataSource.setTestWhileIdle(true);
-		dataSource.setPoolPreparedStatements(false);
-		dataSource.setDefaultAutoCommit(false);
-		return  new DTFDataSourceProxy(dataSource);
+		String type = env.getProperty("spring.datasource.type");
+		switch (type){
+			case "com.alibaba.druid.pool.DruidDataSource":
+				DruidDataSource dataSource = new DruidDataSource();
+				dataSource.setUrl(env.getProperty("spring.datasource.url"));
+				dataSource.setUsername(env.getProperty("spring.datasource.username"));//用户名
+				dataSource.setPassword(env.getProperty("spring.datasource.password"));//密码
+				dataSource.setInitialSize(2);
+				dataSource.setMaxActive(20);
+				dataSource.setMinIdle(0);
+				dataSource.setMaxWait(60000);
+				dataSource.setValidationQuery("SELECT 1");
+				dataSource.setTestOnBorrow(false);
+				dataSource.setTestWhileIdle(true);
+				dataSource.setPoolPreparedStatements(false);
+				dataSource.setDefaultAutoCommit(false);
+				return new DTFDataSourceProxy(dataSource);
+			default:
+				return null;
+		}
 	}
-
-
-
 }
