485b6a6b38ca5907766f3b83dd69a3c6351adcfd
wgy8283335 <wgy8283335@126.com>
2018-10-09 13:35:44 +0800

    initialization

--- dtf-manager/src/main/java/com/coconason/dtf/manager/service/CheckAndSubmitRunnable.java
+++ dtf-manager/src/main/java/com/coconason/dtf/manager/service/CheckAndSubmitRunnable.java
@@ -47,41 +47,36 @@ public final class CheckAndSubmitRunnable implements Runnable{
 
     @Override
     public void run() {
-        try{
-            JSONObject info = JSONObject.parseObject(message.getInfo());
-            String groupId = info.get("groupId").toString();
-            Object obj = info.get("groupMemberSet");
-            TransactionMessageGroupInterface tmfs = obj == null ? messageForSubmitSyncCacheProxy.get(groupId): TransactionMessageFactory.getMessageForSubmitInstance(message);
-            if(tmfs == null||tmfs.getMemberSet().isEmpty()|| messageSyncCacheProxy.get(tmfs.getGroupId())==null){
-                return;
-            }
-            Set setFromMessage =tmfs.getMemberSet();
-            TransactionMessageGroupInterface elementFromCache = messageSyncCacheProxy.get(tmfs.getGroupId());
-            Set setFromCache = elementFromCache.getMemberSet();
-            elementFromCache.setCtxForSubmitting(ctx);
-            messageSyncCacheProxy.put(elementFromCache.getGroupId(),elementFromCache);
-            List<TransactionMessageForAdding> memberList = elementFromCache.getMemberList();
-            //check whether the member from message has the same element as the member from cache.
-            if(!setFromMessage.isEmpty()){
-                if(SetUtil.isSetEqual(setFromMessage,setFromCache)){
-                    for (TransactionMessageForAdding messageForAdding: memberList) {
-                        if(actionType == ActionType.ADD || actionType == ActionType.APPROVESUBMIT){
-                            System.out.println("Send transaction message:\n" + message);
-                            threadPoolForServerProxy.execute(new SendMessageRunnable(elementFromCache.getGroupId()+messageForAdding.getGroupMemberId(),ActionType.APPROVESUBMIT,messageForAdding.getCtx(),"send APPROVESUBMIT message fail", serverThreadLockCacheProxy));
-                        }else{
-                            //success
-                            System.out.println("Send transaction message:\n" + message);
-                            //int i = 6/0;
-                            threadPoolForServerProxy.execute(new SendMessageRunnable(elementFromCache.getGroupId()+messageForAdding.getGroupMemberId(),ActionType.APPROVESUBMIT_STRONG,messageForAdding.getCtx(),"send APPROVESUBMIT_STRONG message fail", serverThreadLockCacheProxy));
-                        }
-                    }
+        JSONObject info = JSONObject.parseObject(message.getInfo());
+        String groupId = info.get("groupId").toString();
+        Object obj = info.get("groupMemberSet");
+        TransactionMessageGroupInterface tmfs = obj == null ? messageForSubmitSyncCacheProxy.get(groupId): TransactionMessageFactory.getMessageForSubmitInstance(message);
+        if(tmfs == null||tmfs.getMemberSet().isEmpty()|| messageSyncCacheProxy.get(tmfs.getGroupId())==null){
+            return;
+        }
+        Set setFromMessage =tmfs.getMemberSet();
+        TransactionMessageGroupInterface elementFromCache = messageSyncCacheProxy.get(tmfs.getGroupId());
+        Set setFromCache = elementFromCache.getMemberSet();
+        elementFromCache.setCtxForSubmitting(ctx);
+        messageSyncCacheProxy.put(elementFromCache.getGroupId(),elementFromCache);
+        List<TransactionMessageForAdding> memberList = elementFromCache.getMemberList();
+        //check whether the member from message has the same element as the member from cache.
+        if(!setFromMessage.isEmpty()){
+            if(SetUtil.isSetEqual(setFromMessage,setFromCache)){
+                for (TransactionMessageForAdding messageForAdding: memberList) {
                     if(actionType == ActionType.ADD || actionType == ActionType.APPROVESUBMIT){
-                        messageSyncCacheProxy.invalidate(tmfs.getGroupId());
+                        System.out.println("Send transaction message:\n" + message);
+                        threadPoolForServerProxy.execute(new SendMessageRunnable(elementFromCache.getGroupId()+messageForAdding.getGroupMemberId(),ActionType.APPROVESUBMIT,messageForAdding.getCtx(),"send APPROVESUBMIT message fail", serverThreadLockCacheProxy));
+                    }else{
+                        //success
+                        System.out.println("Send transaction message:\n" + message);
+                        threadPoolForServerProxy.execute(new SendMessageRunnable(elementFromCache.getGroupId()+messageForAdding.getGroupMemberId(),ActionType.APPROVESUBMIT_STRONG,messageForAdding.getCtx(),"send APPROVESUBMIT_STRONG message fail", serverThreadLockCacheProxy));
                     }
                 }
+                if(actionType == ActionType.ADD || actionType == ActionType.APPROVESUBMIT){
+                    messageSyncCacheProxy.invalidate(tmfs.getGroupId());
+                }
             }
-        }catch (Exception e){
-            e.printStackTrace();
         }
     }
 }
