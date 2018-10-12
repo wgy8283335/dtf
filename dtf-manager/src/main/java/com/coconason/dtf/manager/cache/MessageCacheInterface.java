39acb70658cd7fbca6a18c26d0e7f173420b78d5
wgy8283335 <wgy8283335@126.com>
2018-10-12 17:10:03 +0800

    initialization

--- dtf-manager/src/main/java/com/coconason/dtf/manager/cache/MessageCacheInterface.java
+++ dtf-manager/src/main/java/com/coconason/dtf/manager/cache/MessageCacheInterface.java
@@ -6,6 +6,7 @@ import com.coconason.dtf.manager.message.TransactionMessageGroupInterface;
  * @Author: Jason
  * @date: 2018/10/3-9:32
  */
+
 public interface MessageCacheInterface {
 
     TransactionMessageGroupInterface get(String id);
