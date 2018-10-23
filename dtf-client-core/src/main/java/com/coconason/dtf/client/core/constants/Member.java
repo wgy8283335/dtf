b5da5c74e4d6eb97717a4aad27c17c69fcd58af0
wgy8283335 <wgy8283335@126.com>
2018-10-23 13:43:04 +0800

    initialization

--- dtf-client-core/src/main/java/com/coconason/dtf/client/core/constants/Member.java
+++ dtf-client-core/src/main/java/com/coconason/dtf/client/core/constants/Member.java
@@ -5,7 +5,7 @@ package com.coconason.dtf.client.core.constants;
  * @date: 2018/8/26-11:05
  */
 public final class Member {
-
+    //ORIGINAL_ID is only for transaction creator.
     public static final Long ORIGINAL_ID = 1L;
 
     private Member() {
