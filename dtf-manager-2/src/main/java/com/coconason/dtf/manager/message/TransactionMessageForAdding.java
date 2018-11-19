6ae034f802831879e1ddeb3d9e812f8c0b1ac0e4
wgy8283335 <wgy8283335@126.com>
2018-11-19 09:42:57 +0800

    initialization

---
+++ dtf-manager-2/src/main/java/com/coconason/dtf/manager/message/TransactionMessageForAdding.java
@@ -0,0 +1,47 @@
+package com.coconason.dtf.manager.message;
+
+import io.netty.channel.ChannelHandlerContext;
+
+/**
+ * @Author: Jason
+ * @date: 2018/8/24-17:01
+ */
+public final class TransactionMessageForAdding implements TransactionMessageForAddingInterface {
+    private String groupMemberId;
+    private ChannelHandlerContext ctx;
+    private String method;
+    private String args;
+    private boolean commited = false;
+
+    public TransactionMessageForAdding(String groupMemberId, ChannelHandlerContext ctx, String method, String args) {
+        this.groupMemberId = groupMemberId;
+        this.ctx = ctx;
+        this.method = method;
+        this.args = args;
+    }
+    @Override
+    public String getGroupMemberId() {
+        return groupMemberId;
+    }
+    @Override
+    public ChannelHandlerContext getCtx() {
+        return ctx;
+    }
+    @Override
+    public Boolean isCommitted() {
+        return commited;
+    }
+    @Override
+    public void setCommitted(boolean commited) {
+        this.commited = commited;
+    }
+
+    @Override
+    public String toString() {
+        return "groupMemberId='" + groupMemberId + '\'' +
+                ", ctx=" + ctx +
+                ", method='" + method + '\'' +
+                ", args='" + args + '\'' +
+                ", commited=" + commited;
+    }
+}
