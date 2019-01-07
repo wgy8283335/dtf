731fc8bdba97b61beb0699ff7c1e3e03a92525f0
wgy8283335 <wgy8283335@126.com>
2019-01-07 19:18:41 +0800

    initial

---
+++ dtf-manager-2/src/main/java/com/coconason/dtf/manager/utils/PropertiesReader.java
@@ -0,0 +1,24 @@
+package com.coconason.dtf.manager.utils;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Properties;
+
+/**
+ * @Author: Jason
+ * @date: 2018/9/19-10:50
+ */
+public final class PropertiesReader {
+    private Properties properties;
+
+    public PropertiesReader(String filePath) throws Exception{
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
+        properties = new Properties();
+        properties.load(bufferedReader);
+    }
+
+    public String getProperty(String key){
+        return properties.getProperty(key);
+    }
+
+}
