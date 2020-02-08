7aa41b5ba562c4ba249817599fac825fd9609a90
wangguangyuan <wangguangyuan@sensetime.com>
2020-02-08 20:50:30 +0800

    Add comment in demo.

--- spring-demo/src/main/java/com/coconason/dtf/demo/service/impl/CourseServiceImpl.java
+++ spring-demo/src/main/java/com/coconason/dtf/demo/service/impl/CourseServiceImpl.java
@@ -36,6 +36,7 @@ public class CourseServiceImpl implements ICourseService {
     public DemoResult addCourseInfoWithoutDtf(Course course) throws Exception {
         courseMapper.insertSelective(course);
         Teacher teacher = new Teacher();
+        //
         teacher.setT(1);
         teacher.setTname("Lin");
         System.out.println("before sendPost ---------------------------"+System.currentTimeMillis());
