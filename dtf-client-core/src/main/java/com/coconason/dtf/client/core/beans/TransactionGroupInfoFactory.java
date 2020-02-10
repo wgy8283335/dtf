f9f9a20c9bfcfa3b4fbf7fa41b751900d4c6b809
wgy8283335 <coconason@gmail.com>
2020-02-10 10:31:41 +0800

    Modify code style in TransactionGroupInfoFactory.

--- dtf-client-core/src/main/java/com/coconason/dtf/client/core/beans/TransactionGroupInfoFactory.java
+++ dtf-client-core/src/main/java/com/coconason/dtf/client/core/beans/TransactionGroupInfoFactory.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * Transaction group information factory
+ * Transaction group information factory.
  * 
  * @Author: Jason
  */
@@ -13,31 +13,31 @@ public class TransactionGroupInfoFactory {
     /**
      * Get instance by groupId and memberId.
      * 
-     * @param groupId
-     * @param memberId
+     * @param groupId group id
+     * @param memberId group id
      * @return base transction group information
      */
-    public static BaseTransactionGroupInfo getInstance(String groupId, Long memberId){
+    public static BaseTransactionGroupInfo getInstance(final String groupId, final Long memberId) {
         Set groupMembers = new HashSet<>();
         groupMembers.add(memberId);
         return new TransactionGroupInfo(groupId, memberId, groupMembers);
     }
 
     /**
-     * Get instance by parsing string
+     * Get instance by parsing string.
      * 
-     * @param value
+     * @param value value in string
      * @return base transaction group information
      */
-    public static BaseTransactionGroupInfo getInstanceByParsingString(String value){
+    public static BaseTransactionGroupInfo getInstanceByParsingString(final String value) {
         String[] array = value.split("-");
         String groupId = array[0];
         Long memberId = Long.valueOf(array[1]);
         Set<Long> groupMembers = new HashSet<>();
-        for(int i=2; i<array.length;i++){
+        for (int i = 2; i < array.length; i++) {
             groupMembers.add(Long.valueOf(array[i]));
         }
-        BaseTransactionGroupInfo transactionGroupInfo = new TransactionGroupInfo(groupId,memberId,groupMembers);
+        BaseTransactionGroupInfo transactionGroupInfo = new TransactionGroupInfo(groupId, memberId, groupMembers);
         return transactionGroupInfo;
     }
 }
