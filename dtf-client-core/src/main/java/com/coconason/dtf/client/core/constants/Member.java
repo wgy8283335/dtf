b63a0bf711fa27f871664a0e2b670b2c08dfd5be
wgy8283335 <coconason@gmail.com>
2020-02-10 11:12:24 +0800

    Modify code style in Member class.

--- dtf-client-core/src/main/java/com/coconason/dtf/client/core/constants/Member.java
+++ dtf-client-core/src/main/java/com/coconason/dtf/client/core/constants/Member.java
@@ -1,11 +1,14 @@
 package com.coconason.dtf.client.core.constants;
 
 /**
+ * Member original id.
+ * 
  * @Author: Jason
- * @date: 2018/8/26-11:05
  */
 public final class Member {
-    //ORIGINAL_ID is only for transaction creator.
+    /**
+     * ORIGINAL_ID is only for transaction creator.
+     */
     public static final Long ORIGINAL_ID = 1L;
 
     private Member() {
