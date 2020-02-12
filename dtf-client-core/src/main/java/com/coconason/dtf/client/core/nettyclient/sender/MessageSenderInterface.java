955cd9ae7da42c4fdf1ce997ed83c25b4496afa7
wgy8283335 <coconason@gmail.com>
2020-02-12 11:09:34 +0800

    Modify message sender code style.

--- dtf-client-core/src/main/java/com/coconason/dtf/client/core/nettyclient/sender/MessageSenderInterface.java
+++ dtf-client-core/src/main/java/com/coconason/dtf/client/core/nettyclient/sender/MessageSenderInterface.java
@@ -1,9 +1,11 @@
 package com.coconason.dtf.client.core.nettyclient.sender;
 
 /**
+ * 
+ * 
  * @Author: Jason
- * @date: 2018/10/2-14:00
  */
 public interface MessageSenderInterface {
+    
     void startSendMessage() throws Exception;
 }
--- dtf-client-core/src/main/java/com/coconason/dtf/client/core/nettyclient/sender/TransactionMessageSender.java
+++ dtf-client-core/src/main/java/com/coconason/dtf/client/core/nettyclient/sender/TransactionMessageSender.java
@@ -2,6 +2,8 @@ package com.coconason.dtf.client.core.nettyclient.sender;
 
 import com.coconason.dtf.client.core.beans.BaseTransactionServiceInfo;
 import com.coconason.dtf.client.core.nettyclient.protobufclient.NettyService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -10,45 +12,63 @@ import java.util.Queue;
 import java.util.concurrent.ExecutorService;
 
 /**
+ * Transaction message sender.
+ * 
  * @Author: Jason
- * @date: 2018/8/22-8:31
  */
 
 @Component
-public final class TransactionMessageSender implements MessageSenderInterface{
+public final class TransactionMessageSender implements MessageSenderInterface {
 
+    /**
+     * Logger for TransactionMessageSender class.
+     */
+    private Logger logger = LoggerFactory.getLogger(TransactionMessageSender.class);
+    
+    /**
+     * Queue of transaction message.
+     */
     @Autowired
     @Qualifier("transactionMessageQueueProxy")
     private Queue queue;
-
+    
+    /**
+     * Netty service.
+     */
     @Autowired
     private NettyService service;
-
+    
+    /**
+     * Thread pool.
+     */
     @Autowired
     @Qualifier("threadPoolForClientProxy")
     private ExecutorService threadPoolForClientProxy;
-
+    
+    /**
+     * Start to pull message from queue and send the message.
+     */
     @Override
-    public void startSendMessage() throws InterruptedException{
-        threadPoolForClientProxy.execute(new sendMessageRunnable());
+    public void startSendMessage() throws InterruptedException {
+        threadPoolForClientProxy.execute(new SendMessageRunnable());
     }
-
-    private class sendMessageRunnable implements Runnable {
+    
+    private void sendMessageInQueue() throws InterruptedException {
+        while (true) {
+            BaseTransactionServiceInfo info = (BaseTransactionServiceInfo) queue.poll();
+            service.sendMsg(info);
+        }
+    }
+    
+    private class SendMessageRunnable implements Runnable {
         @Override
         public void run() {
             try {
                 sendMessageInQueue();
-            } catch (Exception e) {
-                e.printStackTrace();
+            } catch (InterruptedException e) {
+                logger.error(e.getMessage());
             }
         }
     }
-
-    private void sendMessageInQueue() throws InterruptedException{
-        while(true){
-            BaseTransactionServiceInfo info = (BaseTransactionServiceInfo)queue.poll();
-            service.sendMsg(info);
-        }
-    }
-
+    
 }
