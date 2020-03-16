fa0eb02214d877d2e7dc3ad6d8ec755d1eeffd92
wgy8283335 <coconason@gmail.com>
2020-03-16 14:26:15 +0800

    Add ManagerServerApplication.

---
+++ dtf-manager/src/main/java/com/coconason/dtf/manager/ManagerServerApplication.java
@@ -0,0 +1,11 @@
+package com.coconason.dtf.manager;
+
+import com.coconason.dtf.manager.protobufserver.NettyServer;
+
+public class ManagerServerApplication {
+    
+    public static void main(final String[] args) throws Exception {
+        NettyServer.main();
+    }
+    
+}
