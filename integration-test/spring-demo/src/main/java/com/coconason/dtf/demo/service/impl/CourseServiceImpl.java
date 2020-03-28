2a192f5bf306178c598cea274d9f16e3e8a070df
wgy8283335 <coconason@gmail.com>
2020-03-28 22:07:16 +0800

    Modify code style.

--- integration-test/spring-demo/src/main/java/com/coconason/dtf/demo/service/impl/CourseServiceImpl.java
+++ integration-test/spring-demo/src/main/java/com/coconason/dtf/demo/service/impl/CourseServiceImpl.java
@@ -19,10 +19,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Random;
-import java.util.UUID;
 
 /**
+ * Course Service.
+ * 
  * @Author: Jason
  * @date: 2018/8/27-15:08
  */
@@ -39,83 +39,118 @@ public class CourseServiceImpl implements ICourseService {
     
     @Autowired
     private RestClientAsync restClientAsync;
-    
+
+    /**
+     * Add course information without distributed transaction.
+     *
+     * @param course Course
+     * @return DemoResult
+     * @throws Exception exception
+     */
     @Override
     @Transactional
-    public DemoResult addCourseInfoWithoutDtf(Course course) throws Exception {
+    public DemoResult addCourseInfoWithoutDtf(final Course course) throws Exception {
         courseMapper.insertSelective(course);
         Teacher teacher = new Teacher();
         // uuid
         teacher.setT(course.getC());
         teacher.setTname("Lin");
-        logger.debug("before sendPost ---------------------------"+System.currentTimeMillis());
-        String result = restClient.sendPost("http://localhost:8082/set_teacher_info_without_dtf",teacher);
-        logger.debug("after sendPost ---------------------------"+System.currentTimeMillis());
+        logger.debug("before sendPost ---------------------------" + System.currentTimeMillis());
+        String result = restClient.sendPost("http://localhost:8082/set_teacher_info_without_dtf", teacher);
+        logger.debug("after sendPost ---------------------------" + System.currentTimeMillis());
         Teacher teacher1 = new Teacher();
         teacher1.setT(course.getC());
         teacher1.setTname("Yun");
-        logger.debug("before 2 sendPost ---------------------------"+System.currentTimeMillis());
-        String result2 = restClient.sendPost("http://localhost:8082/set_teacher_info_without_dtf",teacher1);
-        logger.debug("after 2 sendPost ---------------------------"+System.currentTimeMillis());
+        logger.debug("before 2 sendPost ---------------------------" + System.currentTimeMillis());
+        String result2 = restClient.sendPost("http://localhost:8082/set_teacher_info_without_dtf", teacher1);
+        logger.debug("after 2 sendPost ---------------------------" + System.currentTimeMillis());
         return new DemoResult().ok();
     }
-    
+
+    /**
+     * Add course information with distributed transaction in final synchronization mode.
+     *
+     * @param course Course
+     * @return DemoResult
+     * @throws Exception exception
+     */
     @Override
     @DtfTransaction
     @Transactional
-    public DemoResult addCourseInfo(Course course) {
+    public DemoResult addCourseInfo(final Course course) {
         courseMapper.insertSelective(course);
         Teacher teacher = new Teacher();
         teacher.setT(course.getT());
         teacher.setTname("Lin");
-        logger.debug("before sendPost ---------------------------"+System.currentTimeMillis());
-        String response = restClient.sendPost("http://localhost:8082/set_teacher_info",teacher);
+        logger.debug("before sendPost ---------------------------" + System.currentTimeMillis());
+        String response = restClient.sendPost("http://localhost:8082/set_teacher_info", teacher);
         DemoResult result = new DemoResult(response);
-        if(200 != result.getCode()) {
+        if (200 != result.getCode()) {
             return new DemoResult().build(ErrorCode.SYS_ERROR.value(), ErrorCode.SYS_ERROR.msg());
         }
-        logger.debug("after sendPost ---------------------------"+System.currentTimeMillis());
+        logger.debug("after sendPost ---------------------------" + System.currentTimeMillis());
         return new DemoResult().ok();
     }
-    
+
+    /**
+     * Add course information with distributed transaction in strong synchronization mode.
+     *
+     * @param course Course
+     * @return DemoResult
+     * @throws Exception exception
+     */
     @Override
-    @DtfTransaction(type="SYNC_STRONG")
+    @DtfTransaction(type = "SYNC_STRONG")
     @Transactional
-    public DemoResult addCourseInfoStrong(Course course) throws Exception {
+    public DemoResult addCourseInfoStrong(final Course course) throws Exception {
         courseMapper.insertSelective(course);
         Teacher teacher = new Teacher();
         teacher.setT(course.getT());
         teacher.setTname("Lin");
-        restClient.sendPost("http://localhost:8082/set_teacher_info_strong",teacher);
+        restClient.sendPost("http://localhost:8082/set_teacher_info_strong", teacher);
         Teacher teacher1 = new Teacher();
-        teacher1.setT(course.getT()*10);
+        teacher1.setT(course.getT() * 10);
         teacher1.setTname("Yun");
-        restClient.sendPost("http://localhost:8082/set_teacher_info_strong",teacher1);
+        restClient.sendPost("http://localhost:8082/set_teacher_info_strong", teacher1);
         return new DemoResult().ok();
     }
-    
+
+    /**
+     * Add course information with distributed transaction in asynchronization mode.
+     *
+     * @param course Course
+     * @return DemoResult
+     * @throws Exception exception
+     */
     @Override
-    @DtfTransaction(type="ASYNC_FINAL")
+    @DtfTransaction(type = "ASYNC_FINAL")
     @Transactional
-    public DemoResult addCourseInfoAsync(Course course) throws Exception {
+    public DemoResult addCourseInfoAsync(final Course course) throws Exception {
         courseMapper.insert(course);
         //int kk = 6/0;
         Teacher teacher = new Teacher();
         teacher.setT(course.getT());
         teacher.setTname("Yun");
-        restClientAsync.sendPost("http://localhost:8082/set_teacher_info_async",teacher);
+        restClientAsync.sendPost("http://localhost:8082/set_teacher_info_async", teacher);
         Sc sc = new Sc();
         sc.setC(course.getC());
         sc.setS(course.getC());
         sc.setScore(95);
-        restClientAsync.sendPost("http://localhost:8083/add_sc_info_async",sc);
+        restClientAsync.sendPost("http://localhost:8083/add_sc_info_async", sc);
         return new DemoResult().ok();
     }
-    
+
+    /**
+     * Get course information.
+     *
+     * @param id integer
+     * @return DemoResult
+     * @throws Exception exception
+     */
     @Override
     @DtfTransaction
-    @Transactional(readOnly=true)
-    public DemoResult getCourseInfo(int id) throws Exception{
+    @Transactional(readOnly = true)
+    public DemoResult getCourseInfo(final int id) throws Exception {
         CourseExample courseExample = new CourseExample();
         CourseExample.Criteria criteria = courseExample.createCriteria();
         criteria.andCEqualTo(id);
@@ -123,14 +158,21 @@ public class CourseServiceImpl implements ICourseService {
         Course course = courseList.get(0);
         Integer teacherId = course.getT();
         //int kk = 6/0;
-        String result = restClient.sendGet("http://localhost:8082/get_teacher_info?id={1}",teacherId);
+        String result = restClient.sendGet("http://localhost:8082/get_teacher_info?id={1}", teacherId);
         JSONObject map = JSONObject.parseObject(result);
         DemoResult demoResult = new DemoResult().ok(map.get("datum"));
-        return  demoResult;
+        return demoResult;
     }
-    
+
+    /**
+     * Get course.
+     *
+     * @param id integer
+     * @return DemoResult
+     * @throws Exception exception
+     */
     @Override
-    public Course getCourse(int id) throws Exception {
+    public Course getCourse(final int id) throws Exception {
         CourseExample courseExample = new CourseExample();
         CourseExample.Criteria criteria = courseExample.createCriteria();
         criteria.andCEqualTo(id);
