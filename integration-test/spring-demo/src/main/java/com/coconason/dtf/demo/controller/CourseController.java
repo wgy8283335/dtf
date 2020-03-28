91cb911f2569029d1cc9fa5d1cf0935dd75fe442
wgy8283335 <coconason@gmail.com>
2020-03-28 21:33:17 +0800

    Modify code style.

--- integration-test/spring-demo/src/main/java/com/coconason/dtf/demo/controller/CourseController.java
+++ integration-test/spring-demo/src/main/java/com/coconason/dtf/demo/controller/CourseController.java
@@ -11,8 +11,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
 /**
+ * Course Controller.
+ * 
  * @Author: Jason
  * @date: 2018/8/27-15:00
  */
@@ -20,40 +21,82 @@ import org.springframework.web.bind.annotation.RestController;
 public class CourseController {
     
     @Autowired
-    ICourseService courseService;
+    private ICourseService courseService;
     
-    @RequestMapping(value = "/add_course_info_without_dtf",method = RequestMethod.POST)
-    public DemoResult addCourseInfoWithoutDtf(@RequestBody Course course) throws Exception {
+    /**
+     * Add course information without distributed transaction.
+     * 
+     * @param course Course
+     * @return DemoResult
+     * @throws Exception exception
+     */
+    @RequestMapping(value = "/add_course_info_without_dtf", method = RequestMethod.POST)
+    public DemoResult addCourseInfoWithoutDtf(final @RequestBody Course course) throws Exception {
         DemoResult demoResult = courseService.addCourseInfoWithoutDtf(course);
         return demoResult;
     }
-    
-    @RequestMapping(value = "/add_course_info",method = RequestMethod.POST)
-    public DemoResult addCourseInfo(@RequestBody Course course) throws Exception {
+
+    /**
+     * Add course information with distributed transaction in final synchronization mode.
+     * 
+     * @param course Course
+     * @return DemoResult
+     * @throws Exception exception
+     */
+    @RequestMapping(value = "/add_course_info", method = RequestMethod.POST)
+    public DemoResult addCourseInfo(final @RequestBody Course course) throws Exception {
         DemoResult demoResult = courseService.addCourseInfo(course);
         return demoResult;
     }
-    
-    @RequestMapping(value = "/add_course_info_strong",method = RequestMethod.POST)
-    public DemoResult addCourseInfoStrong(@RequestBody Course course) throws Exception {
+
+    /**
+     * Add course information with distributed transaction in strong synchronization mode.
+     * 
+     * @param course Course
+     * @return DemoResult
+     * @throws Exception exception
+     */
+    @RequestMapping(value = "/add_course_info_strong", method = RequestMethod.POST)
+    public DemoResult addCourseInfoStrong(final @RequestBody Course course) throws Exception {
         DemoResult demoResult = courseService.addCourseInfoStrong(course);
         return demoResult;
     }
-    
-    @RequestMapping(value = "/add_course_info_async",method = RequestMethod.POST)
-    public DemoResult addCourseInfoAsync(@RequestBody Course course) throws Exception {
+
+    /**
+     * Add course information with distributed transaction in asynchronization mode.
+     * 
+     * @param course Course
+     * @return DemoResult
+     * @throws Exception exception
+     */
+    @RequestMapping(value = "/add_course_info_async", method = RequestMethod.POST)
+    public DemoResult addCourseInfoAsync(final @RequestBody Course course) throws Exception {
         DemoResult demoResult = courseService.addCourseInfoAsync(course);
         return demoResult;
     }
-    
-    @RequestMapping(value = "/get_course_info",method = RequestMethod.GET)
-    public DemoResult getCourseInfo(@RequestParam int id) throws Exception {
+
+    /**
+     * Get course information.
+     * 
+     * @param id id of course
+     * @return DemoResult
+     * @throws Exception exception
+     */
+    @RequestMapping(value = "/get_course_info", method = RequestMethod.GET)
+    public DemoResult getCourseInfo(final @RequestParam int id) throws Exception {
         DemoResult<Teacher> demoResult = courseService.getCourseInfo(id);
         return demoResult;
     }
-    
-    @RequestMapping(value = "/get_course",method = RequestMethod.GET)
-    public DemoResult getCourse(@RequestParam int id) throws Exception {
+
+    /**
+     * Get course.
+     * 
+     * @param id id of course
+     * @return DemoResult
+     * @throws Exception exception
+     */
+    @RequestMapping(value = "/get_course", method = RequestMethod.GET)
+    public DemoResult getCourse(final @RequestParam int id) throws Exception {
         Course course = courseService.getCourse(id);
         return new DemoResult().ok(course);
     }
